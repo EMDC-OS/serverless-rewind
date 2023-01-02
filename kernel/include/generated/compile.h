0183f8e87a1620a81e85e54600e46755006bde81
user01 <bumsuk.kim@csi.skku.edu>
2023-01-02 04:40:49 +0000

    OSDI23 Kernel

---
+++ kernel/include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 349 */
+/* SMP */
+#define UTS_MACHINE "x86_64"
+#define UTS_VERSION "#349 SMP Mon Dec 12 13:33:57 UTC 2022"
+#define LINUX_COMPILE_BY "root"
+#define LINUX_COMPILE_HOST "r740-rewind"
+#define LINUX_COMPILER "gcc version 9.4.0 (Ubuntu 9.4.0-1ubuntu1~20.04.1)"
