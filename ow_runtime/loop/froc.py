f9be436066a03362d52023f8c781fa16780020e4
jhsong27 <jaehyun.song@csi.skku.edu>
2024-12-31 04:28:37 +0000

    loop runtime modification

--- ow_runtime/loop/froc.py
+++ ow_runtime/loop/froc.py
@@ -15,7 +15,6 @@ func = None
 syscall = ctypes.CDLL(None).syscall
 
 def init_func(fname):
-    #debug("Into the init_func()...\n")
     global func
     if (os.path.isfile(lib_name) and os.access(lib_name, os.R_OK)):
         # Function lib init
@@ -36,18 +35,9 @@ if recvData == 'init':
 else:
     quit()
 
-#fds = os.popen("ls -al /proc/"+str(os.getpid())+"/fd >> /root/ow_test/prev").read()
-#syscall(551)
 while True:
     param = sock.recv(1024).decode('utf-8')
-
-    #rss = os.popen("cat /proc/"+str(os.getpid())+"/status | grep RSS").read()
     result = func.main(json.loads(param))
-    #result["RSS-0"] = rss
-    #result["RSS"] = os.popen("cat /proc/"+str(os.getpid())+"/status | grep RSS").read()
     result["RSS"] = resource.getrusage(resource.RUSAGE_SELF).ru_maxrss
-    #dump = os.popen("ls -al /proc/"+str(os.getpid())+"/fd >> /root/ow_test/then").read()
-    #time.sleep(2)
     sendData = json.dumps(result)
     sock.send(sendData.encode('utf-8'))
-    #syscall(551)
